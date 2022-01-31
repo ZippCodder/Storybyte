8b4d82cbda15fd8e2f745c7f1be2bfa87f431f80
ZippCodder <deonisawewsome777@gmail.com>
2022-01-31 21:20:55 +0000

    adding pages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const pug = require("pug");
 const path = require("path");
+const { MongoClient } = require("mongodb");
 
 console.log(pug.renderFile(path.resolve("./public/index.pug")))
